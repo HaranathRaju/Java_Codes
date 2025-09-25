580c3e3b1d81cbc7240956b31d83e1fba683b9bf
HaranathRaju <125989712+HaranathRaju@users.noreply.github.com>
2025-09-25 10:08:53 +0530

    Update LongestSubstring.java

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -1,31 +1,29 @@
-import java.util.HashSet;
-import java.util.Scanner;
-public class LongestSubstring {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("enter the string: ");
-        String s=sc.nextLine();
-        int maxlen=0;
-        String substring="";
-        HashSet<Character> set=new HashSet<>();
+import java.util.*;
 
-        for (int i=0;i<s.length();i++) {
-            String temp="";
-            for (int j=0;j<s.length();j++) {
-                char c=s.charAt(j);
-                if (!set.contains(c)) {
-                    set.add(c);
-                    temp+=c;
-                } else {
-                    break;
-                }
+public class LongestSubstringMap {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, maxLen = 0;
+        String longest = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (map.containsKey(c) && map.get(c) >= left) {
+                left = map.get(c) + 1;
             }
-            if (temp.length()>maxlen) {
-                maxlen=temp.length();
-                substring=temp;
+
+            map.put(c, i);
+
+            if (i - left + 1 > maxLen) {
+                maxLen = i - left + 1;
+                longest = s.substring(left, i + 1);
             }
         }
-        System.out.println("maximum length of substring is: "+maxlen);
-        System.out.println("the longest substring is: "+substring);
+
+        System.out.println("Longest substring: " + longest);
+        System.out.println("Length: " + maxLen);
     }
 }
