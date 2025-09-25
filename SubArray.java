dab1538e0a8b91fdccf6dd9baf7eb47dbbd0db38
HaranathRaju <125989712+HaranathRaju@users.noreply.github.com>
2025-09-25 15:56:14 +0530

    Update SubArray.java

--- SubArray.java
+++ SubArray.java
@@ -1,23 +1,17 @@
+import java.util.Arrays;
 
 public class SubArray {
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 7, 5 };
-        int target = 12;
-        for (int i = 0; i < arr.length; i++) {
-            int sum = arr[i];
-            for (int j = i + 1; j < arr.length; j++) {
-                sum += arr[j];
-                if (sum == target) {
-                    System.out.print("subarray: ");
-                    for (int k = i; k <= j; k++) {
-                        System.out.print(arr[k] + " ");
-                    }
-                    System.out.println();
+        int arr []={1,2,3,7,5};
+        int target=12;
+        for (int i=0;i<arr.length;i++) {
+            int sum=0;
+            for (int j=i;j<arr.length;j++) {
+                sum+=arr[j];
+                if (sum==target) {
+                    System.out.println(Arrays.toString(Arrays.copyOfRange(arr, i, j+1))); 
                 }
             }
-
-        }
-
+        }   
     }
-
 }
